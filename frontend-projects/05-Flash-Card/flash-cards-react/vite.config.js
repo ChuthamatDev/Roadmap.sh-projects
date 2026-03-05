8bd31ce61201f234d4026ba7202f1d7c025bb13c
Pharew Chuthamat <chuthamatbuaban@gmail.com>
2026-03-05 21:39:12 +0700

    fix: correct base path to include /dist/ directory

--- frontend-projects/05-Flash-Card/flash-cards-react/vite.config.js
+++ frontend-projects/05-Flash-Card/flash-cards-react/vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/frontend-projects/05-Flash-Card/flash-cards-react/',
+    base: '/frontend-projects/05-Flash-Card/flash-cards-react/dist/',
     plugins: [react(), tailwindcss()],
 })
